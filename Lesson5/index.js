fc5a27714f4b1ba702771059f004a8859300fe2c
KapicAlex <alexkapic@gmail.com>
2021-03-04 00:13:08 +0200

    Homework#5(correct index.js)

--- Lesson5/index.js
+++ Lesson5/index.js
@@ -7,14 +7,14 @@ playPause.addEventListener("click", function (e) {
   if (video.paused || video.ended) {
     video.play();
     playPause.style.background =
-      'url("/images/video_pause_button.png") center no-repeat, rgba(0, 0, 0, 0.5)';
+      'url("images/video_pause_button.webp") center no-repeat, rgba(0, 0, 0, 0.5)';
     videoTitle.style.visibility = "hidden";
     videoDescription.style.visibility = "hidden";
     videoTimes.style.visibility = "hidden";
   } else {
     video.pause();
     playPause.style.background =
-      'url("/images/video_play_button.png") center no-repeat, rgba(0, 0, 0, 0.5)';
+      'url("images/video_play_button.webp") center no-repeat, rgba(0, 0, 0, 0.5)';
     videoTitle.style.visibility = "visible";
     videoDescription.style.visibility = "visible";
     videoTimes.style.visibility = "visible";
